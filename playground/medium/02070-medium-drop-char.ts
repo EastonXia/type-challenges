9d4455788f82040ae0674e34a68628e4b41d2129
EastonShay <a386314080@qq.com>
2024-12-11 12:30:18 +0800

    更新medium-2070

--- playground/medium/02070-medium-drop-char.ts
+++ playground/medium/02070-medium-drop-char.ts
@@ -18,7 +18,9 @@
 
 /* _____________ 你的代码 _____________ */
 
-type DropChar<S, C> = any
+type DropChar<S extends string, C extends string> = S extends `${infer F}${infer R}` ?
+  F extends C ? DropChar<R, C> : `${F}${DropChar<R, C>}`
+  : S
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'

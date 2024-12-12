d0e3efc8449f7aab2aa69a5c583a76944405c223
EastonShay <a386314080@qq.com>
2024-12-12 12:35:32 +0800

    更新medium-2257

--- playground/medium/02257-medium-minusone.ts
+++ playground/medium/02257-medium-minusone.ts
@@ -19,7 +19,26 @@
 
 /* _____________ 你的代码 _____________ */
 
-type MinusOne<T extends number> = any
+// 不是人写的，这么夸张
+type ParseInt<T extends string> = T extends `${infer Digit extends number}` ? Digit : never
+type ReverseString<S extends string> = S extends `${infer First}${infer Rest}` ? `${ReverseString<Rest>}${First}` : ''
+type RemoveLeadingZeros<S extends string> = S extends '0' ? S : S extends `${'0'}${infer R}` ? RemoveLeadingZeros<R> : S
+type InternalMinusOne<
+  S extends string,
+> = S extends `${infer Digit extends number}${infer Rest}` ?
+  Digit extends 0 ?
+      `9${InternalMinusOne<Rest>}` :
+    `${[9, 0, 1, 2, 3, 4, 5, 6, 7, 8][Digit]}${Rest}` :
+  never
+
+// 总体思路，转为字符串后再操作
+// 1.转字符串，然后反转。在类型操作中，从左往右运算更好写一点
+// 2.进行减 1 操作，分两种情况，一种是为 ‘0’ ，一种是非 ‘0’。
+//   - 为 ‘0’ 时，当前数结果为 9，然后还要对下一位运算。
+//   - 非 ‘0’ 时，直接算减 1 的结果，因为 ts 不能数字类型运算，所以要借助数组和数组下标
+// 3.完成计算后反转回来，并且去掉头部为 0 的数字
+// 4.把字符串转回数字
+type MinusOne<T extends number> = ParseInt<RemoveLeadingZeros<ReverseString<InternalMinusOne<ReverseString<`${T}`>>>>>
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'

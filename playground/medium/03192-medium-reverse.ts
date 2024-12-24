0b927aea940361a646187bcf79b283191f9d2b86
EastonShay <a386314080@qq.com>
2024-12-24 13:54:05 +0800

    更新medium-3192

--- playground/medium/03192-medium-reverse.ts
+++ playground/medium/03192-medium-reverse.ts
@@ -19,7 +19,7 @@
 
 /* _____________ 你的代码 _____________ */
 
-type Reverse<T> = any
+type Reverse<T extends any[]> = T extends [infer F, ...infer Rest] ? [...Reverse<Rest>, F] : []
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'

0cd1c5f258f55f5406bfc6dfd91e6d9d50ac64e2
EastonShay <a386314080@qq.com>
2024-12-28 12:37:18 +0800

    更新medium-3243

--- playground/medium/03243-medium-flattendepth.ts
+++ playground/medium/03243-medium-flattendepth.ts
@@ -21,7 +21,15 @@
 
 /* _____________ 你的代码 _____________ */
 
-type FlattenDepth = any
+// 考虑下层数问题，ts 中需要比较具体的数字类型，通常都需要数组的 length 属性。
+// 那么我们可以增加一个数组类型参数 U，每次打平向它里面添加一个元素来达到 ”+1“ 的目的。
+// 然后每次递归时，判断层数和它的 length 是否一致，如果一致，说明打平层数够了，直接返回本身即可；否则继续递归。
+type FlattenDepth<T extends any[], Depth extends number = 1, U extends any[] = []> = U['length'] extends Depth
+  ? T
+  : T extends [infer First, ...infer Rest]
+    // 深度递归，考虑到本题的性质，不建议选择单个遍历递归 FlattenDepth<[...First, ...Rest], Depth>, 应选择多向递归
+    ? First extends any[] ? [...FlattenDepth<First, Depth, [...U, 1]>, ...FlattenDepth<Rest, Depth, U>] : [First, ...FlattenDepth<[...Rest], Depth, U>]
+    : []
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
